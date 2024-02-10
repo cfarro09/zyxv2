fe8840832f4631e755272599b32815199640d881
Pablo Rojas <projasb25@gmail.com>
2024-02-10 12:59:51 -0500

    downgrade reduct-redux version"

--- src/pages/Login/Index.tsx
+++ src/pages/Login/Index.tsx
@@ -45,7 +45,7 @@ export default function SignIn() {
 
         // const token = await recaptchaRef?.current?.executeAsync();
         // setshowError(true);
-        dispatch(login("", "") as any);
+        dispatch(login("", ""));
         // console.log({
         //     email: data.get('email'),
         //     password: data.get('password'),
